4a404123d9cb36792b595d7f82b43c22ee36a966
Andika Pranayoga <praandikayoga@gmail.com>
2021-07-12 20:50:26 +0800

    Update Halaman Quesioner

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Fungsi autofocus Checkbox Input
+document.getElementById("ck_other").addEventListener('click',function(){
+    let input_other = document.getElementById("input_other");
+    input_other.focus();
+});
