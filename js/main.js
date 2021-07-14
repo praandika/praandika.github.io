4660ac82443e4caf15967984d64755e0ad6b4807
Andika Pranayoga <praandikayoga@gmail.com>
2021-07-14 15:23:55 +0800

    Tambah Halaman Login dan Register

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ document.getElementById("ck_other").addEventListener('click',function(){
     let input_other = document.getElementById("input_other");
     input_other.focus();
 });
+
+// Tampilkan menu User
+function tampilMenu(){
+    let show = document.getElementById("menuakun").classList.toggle("show");
+    return show;
+}
