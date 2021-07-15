67c7a25e1874c5bf6fca052fb2760a1e0dfd8ec3
Andika Pranayoga <praandikayoga@gmail.com>
2021-07-15 18:33:05 +0800

    Membuat Halaman Data

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@ document.getElementById("ck_other").addEventListener('click',function(){
 
 // Tampilkan menu User
 function tampilMenu(){
-    let show = document.getElementById("menuakun").classList.toggle("show");
+    let show = document.getElementById("menuakun");
+    if (show.style.display === "block") {
+        show.style.display = "none";
+    }else{
+        show.style.display = "block";
+    }
+    console.log('sukses');
     return show;
+    
+}
+
+// Tampilkan menu shortcut
+function tampilShortcut(){
+    let shortcut = document.getElementById("container-shortcut");
+    if (shortcut.style.display === "block") {
+        shortcut.style.display = "none";
+    }else{
+        shortcut.style.display = "block";
+    }
+    console.log('sukses');
+    return shortcut;
 }
